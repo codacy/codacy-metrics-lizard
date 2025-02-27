71bf1c74c38d8e840ee71ed0eabc16c8533cc0e2
Stefan Vacareanu <stefan@codacy.com>
2025-02-27 17:54:52 +0200

    update

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { run } from "codacy-seed"
-
+// run from codacy-seed aka codacy-egine-typescript-seed is working for tool results, but not for metrics
+// a new class must be implemented in order to run and manage metrics results, either here, or in the seed
+// that is managing MetricsResults the same way ToolResults are managed
 import { lizardMetricsEngine } from "./lizardMetricsEngine"
 
 run(lizardMetricsEngine())
