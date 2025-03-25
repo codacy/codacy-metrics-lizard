33582484ae3678fafadfe09c0b9a4d0b34e5e891
Stefan Vacareanu <stefan@codacy.com>
2025-03-25 18:13:36 +0200

    V1

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
-import { run } from "codacy-seed"
-// run from codacy-seed aka codacy-egine-typescript-seed is working for tool results, but not for metrics
-// a new class must be implemented in order to run and manage metrics results, either here, or in the seed
-// that is managing MetricsResults the same way ToolResults are managed
 import { lizardMetricsEngine } from "./lizardMetricsEngine"
+import { parseTimeoutSeconds } from "./parseTimeoutSeconds"
 
-run(lizardMetricsEngine())
+const timeoutSeconds = parseTimeoutSeconds(process.env.TIMEOUT_SECONDS)
+
+const timeoutHandle = setTimeout(() => {
+    console.error("Timeout occurred. Exiting.")
+    process.exit(2)
+}, timeoutSeconds * 1000)
+
+async function run() {
+    const results = await lizardMetricsEngine()
+    console.log(results)
+}
+
+run()
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
+    .finally(() => clearTimeout(timeoutHandle))
