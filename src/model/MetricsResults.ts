f13d23e9792bff288eb553498a4a562d03239cfe
Stefan Vacareanu <stefan@codacy.com>
2025-03-26 16:28:51 +0200

    V4

--- src/model/MetricsResults.ts
+++ src/model/MetricsResults.ts
@@ -1,10 +1,4 @@
-export interface LineComplexity {
-  "line": number;
-  "value": number;
-}
-
 export interface FileComplexity {
   "filename": string;
   "complexity": number; // this should be the sum of method complexities from a file
-  "lineComplexities": LineComplexity[];
 }
